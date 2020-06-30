05b977058ddf6573eddec5f70244ef4c286b3b0c
PaoDerDoktor <paoderdoktor@gmail.com>
2020-06-30 02:56:13 +0200

    Added a coder-side script to fetch translations

---
+++ localScripts/getSC2020Translations.ps1
@@ -0,0 +1,9 @@
+$translatedAmount = 14
+$translationPath = "../summercode/2020/translations/"
+
+# Getting days translations
+for ($i = 0; $i -lt $translatedAmount; $i++) {
+    $url = "https://raw.githubusercontent.com/OpenHiveFR/aoc2019FR/master/day"+$i+"/day"+$i+".html"
+    $outPath = $translationPath+"day/"+$i+".html"
+    Invoke-WebRequest $url -OutFile $outPath
+}
