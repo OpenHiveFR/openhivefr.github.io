bca0c50e09dc77af74d8ea961da5f2fad307507f
PaoDerDoktor <PaoDerDoktor@gmail.com>
2020-12-01 13:46:32 +0100

    Added AoC2020 Translation

---
+++ js/scripts/fillDaysMenu2.js
@@ -0,0 +1,48 @@
+let daysMenu = document.getElementById("daysMenu");
+
+let translatedDays = 1;
+
+for (let i = 0; i <= translatedDays; i++) {
+    let newButton = document.createElement("BUTTON");
+    
+    newButton.setAttribute("class", "dayButton");
+    
+    newButton.dataset.day = i;
+    
+    newButton.addEventListener("click", function(event) {
+        event.preventDefault();
+        
+        localStorage.setItem('selectedDay', i);
+        
+        let translationIFrame = document.getElementById("translation");
+        let aocIFrame = document.getElementById("aoc");
+
+        if (i == 0) {
+            aocIFrame.setAttribute("src", "https://adventofcode.com/2020/about");
+        } else {
+           aocIFrame.setAttribute("src", "https://adventofcode.com/2020/day/"+event.target.dataset.day); 
+        }
+        
+        translationIFrame.setAttribute("src", "translations/day/"+event.target.dataset.day+".html");
+        
+        let currentlyClicked = document.getElementById("clicked");
+        
+        if (currentlyClicked != null) {
+            currentlyClicked.removeAttribute("id");
+        }
+
+        event.target.setAttribute("id", "clicked");
+    });
+    
+    newButtonText = document.createTextNode("["+i+"]");
+
+    newButton.appendChild(newButtonText);
+
+    daysMenu.appendChild(newButton);
+}
+
+if (localStorage.getItem('selectedDay') !== null) {
+    daysMenu.children[localStorage.getItem('selectedDay')].click();
+} else {
+    daysMenu.children[0].click();
+}
---
+++ localScripts/getAoC2020Translations.ps1
@@ -0,0 +1,9 @@
+$translatedAmount = 2
+$translationPath = "../santacode/2020/translations/"
+
+# Getting days translations
+for ($i = 1; $i -lt $translatedAmount; $i++) {
+    $url = "https://raw.githubusercontent.com/OpenHiveFR/aoc2020FR/main/day"+$i+"/day"+$i+".html"
+    $outPath = $translationPath+"day/"+$i+".html"
+    Invoke-WebRequest $url -OutFile $outPath
+}
